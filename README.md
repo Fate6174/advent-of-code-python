b011229738cb1de2701648ef64ceffceaf5f36e5
Henning Sauter <henning.sauter@outlook.com>
2025-12-01 19:19:46 +0100

    day 01

--- README.md
+++ README.md
@@ -1,22 +1,7 @@
-# Advent of code 2024
+# Advent of code 2025
 
 Problems list:
 
-- [Day 1: Historian Hysteria](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/01.py)
-- [Day 2: Red-Nosed Reports](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/02.py)
-- [Day 3: Mull It Over](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/03.py)
-- [Day 4: Ceres Search](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/04.py)
-- [Day 5: Print Queue](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/05.py)
-- [Day 6: Guard Gallivant](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/06.py)
-- [Day 7: Bridge Repair](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/07.py)
-- [Day 8: Resonant Collinearity](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/08.py)
-- [Day 9: Disk Fragmenter](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/09.py)
-- [Day 10: Hoof It](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/10.py)
-- [Day 11: Plutonian Pebbles](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/11.py)
-- [Day 12: Garden Groups](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/12.py)
-- [Day 13: Claw Contraption](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/13.py)
-- [Day 14: Restroom Redoubt](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/14.py)
-- [Day 15: Warehouse Woes](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/15.py)
-- [Day 19: Linen Layout](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/19.py)
+- [Day 1: Secret Entrance](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/01.py)
 
 Created via: [advent-of-code-setup](https://github.com/tomfran/advent-of-code-setup)
--- src/01.py
+++ src/01.py
@@ -1,19 +1,74 @@
 from utils.api import get_input
 
 input_str = get_input(1)
+# input_str = """L68
+# L30
+# R48
+# L5
+# R60
+# L55
+# L1
+# L99
+# R14
+# L82"""
 
 # WRITE YOUR SOLUTION HERE
 
 # part 1
-l1 = list(sorted([int(line.split(' ')[0]) for line in input_str.splitlines()]))
-l2 = list(sorted([int(line.split(' ')[3]) for line in input_str.splitlines()]))
+dial_state = 50
+num_at_zero = 0
+for rotation in input_str.splitlines():
+    # parse rotation
+    try:
+        direction = rotation[0]
+        distance = int(rotation[1:])
+        assert direction in ['L', 'R']
+    except:
+        raise ValueError(f'Could not parse rotation: {rotation}')
+    if direction == 'L':
+        distance = -distance
+    old_dial_state = dial_state
+    dial_state = (dial_state + distance) % 100
+    if dial_state == 0:
+        ended_at_zero_str = ' Ended at zero.'
+        num_at_zero += 1
+    else:
+        ended_at_zero_str = ''
+    print(f'{old_dial_state} rotated by {rotation} -> {old_dial_state + distance} -> {dial_state}.' + ended_at_zero_str)
 
-res = sum([abs(x-y) for (x,y) in zip(l1,l2)])
-print(f'part1: {res}')
+print(f'The dial was {num_at_zero} times at the position 0 at the end of an rotation.')
+print()
 
 # part 2
-l1 = [int(line.split(' ')[0]) for line in input_str.splitlines()]
-l2 = [int(line.split(' ')[3]) for line in input_str.splitlines()]
+dial_state = 50
+num_at_zero = 0
+for rotation in input_str.splitlines():
+    # parse rotation
+    try:
+        direction = rotation[0]
+        distance = int(rotation[1:])
+        assert direction in ['L', 'R']
+    except:
+        raise ValueError(f'Could not parse rotation: {rotation}')
+    
+    if direction == 'L':
+        distance = -distance
+    
+    old_dial_state = dial_state
+    old_num_at_zero = num_at_zero
+    
+    dial_state = (old_dial_state + distance) % 100
+    total_revolutions = abs(distance) // 100
+    num_at_zero += total_revolutions
+    if any([direction == 'R' and dial_state < old_dial_state,
+            direction == 'L' and old_dial_state != 0 and dial_state > old_dial_state,
+            direction == 'L' and dial_state == 0]):
+        num_at_zero += 1
+    if num_at_zero > old_num_at_zero:
+        traversals = num_at_zero - old_num_at_zero
+        traverse_zero_str = f' Traversed zero {'once' if traversals == 1 else str(traversals) + ' times'}.'
+    else:
+        traverse_zero_str = ''
+    print(f'{old_dial_state} rotated by {rotation} -> {old_dial_state + distance} -> {dial_state}.' + traverse_zero_str)
 
-res = sum([x if x==y else 0 for x in l1 for y in l2])
-print(f'part2: {res}')
+print(f'The dial was {num_at_zero} times at the position 0 during or at the end of an rotation.')
