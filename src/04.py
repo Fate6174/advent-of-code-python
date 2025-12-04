9a8a873961272dbab551abad036b48cb4d454be7
Henning Sauter <henning.sauter@outlook.com>
2025-12-04 18:41:58 +0100

    improved day 04 by changing from numpy array grid representation to a set

--- src/04.py
+++ src/04.py
@@ -1,7 +1,5 @@
 from utils.api import get_input
 from time import time
-import numpy as np
-import numpy.typing as npt
 
 input_str = get_input(4)
 # input_str = """..@@.@@@@.
@@ -18,43 +16,45 @@ input_str = get_input(4)
 
 # WRITE YOUR SOLUTION HERE
 
-def find_removables(grid : npt.NDArray) -> list[tuple[int,int]]:
-    m, n = grid.shape
+def find_removables(grid_set : set[complex]) -> list[complex]:
     out = []
-    for row_idx in range(m):
-        for col_idx in range(n):
-            if grid[row_idx,col_idx] == '@':
-                # count adjacent paper rolls (@)
-                adjacent_rolls = 0
-                for i in [-1,0,1]:
-                    if row_idx + i in [-1,m]:
-                        continue
-                    for j in [-1,0,1]:
-                        if col_idx + j in [-1,n]:
-                            continue
-                        if (i,j) != (0,0) and grid[row_idx+i,col_idx+j] == '@':
-                            adjacent_rolls += 1
-                if adjacent_rolls < 4:
-                    out += [(row_idx,col_idx)]
+    for coordinate in grid_set:
+        # count adjacent paper rolls (@)
+        adjacent_rolls = 0
+        for dz in [-1,1,1j,-1j,1+1j,1-1j,-1+1j,-1-1j]:
+            if coordinate + dz in grid_set:
+                adjacent_rolls += 1
+        if adjacent_rolls < 4:
+            out += [coordinate]
     return out
 
 # part 1
 tic = time()
-grid = np.array([list(row) for row in input_str.splitlines()])
-out = len(find_removables(grid))
+# parse input into set containining the coordinates of paper rolls (@)
+grid_set = set()
+for row_idx, row in enumerate(input_str.splitlines()):
+    for col_idx, item in enumerate(row):
+        if item == '@':
+            grid_set.add(row_idx+col_idx*1j)
+out = len(find_removables(grid_set))
 toc = time()
 print(f'{toc-tic:.6f} secs: {out}')
 
 # part 2
 tic = time()
-grid = np.array([list(row) for row in input_str.splitlines()])
+# parse input into set containining the coordinates of paper rolls (@)
+grid_set = set()
+for row_idx, row in enumerate(input_str.splitlines()):
+    for col_idx, item in enumerate(row):
+        if item == '@':
+            grid_set.add(row_idx+col_idx*1j)
 out = 0
 while True:
-    removables = find_removables(grid)
+    removables = find_removables(grid_set)
     if removables == []:
         break
-    for (row_idx, col_idx) in removables:
-        grid[row_idx][col_idx] = '.'
+    for coordinate in removables:
+        grid_set.remove(coordinate)
     out += len(removables)
 toc = time()
 print(f'{toc-tic:.6f} secs: {out}')
